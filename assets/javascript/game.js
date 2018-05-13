73da62d6eb583cb283f013b4d69400465b61caee
drewphillips <drew@superheroprinting.com>
2018-05-12 20:26:50 -0700

    working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,34 +3,41 @@
         var winstotal = 0;
         var losestotal = 0;
         var guessesLeftTotal = 10;
-        var totalGuesses = 0;
+        var computerGuess = "";
     
         var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+
+        function resetGame(){
+            computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+            console.log(computerGuess)
+            guessesLeftTotal = 10;
+
+        }    
+        resetGame()
     
         document.onkeyup = function (event) {
             var playerGuess = event.key;
-            var computerGuess = computerChoices[Math.floor(Math.random() * alphabet.length)];
-            userGuess.push(alphabet);
-            console.log(computerGuess[0]);
-    
-    
+            
             //8. When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
             //9. When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins). 
     
-            if (userGuess === computerChoice) {
+            if (playerGuess === computerGuess) {
                 winstotal++;
+                resetGame()
+                
             } else {
-                guessesLeftTotal--;
+                guessesLeftTotal--; 
             }
     
             if (guessesLeftTotal === 0) {
-                lossestotal++
+                losestotal++
+                resetGame()
             }
     
             document.getElementById('wins').innerHTML = "Wins: " + winstotal;
-            document.getElementById('losses').innerHTML = "losses: " + lossestotal;
-            document.getElementById('totalGuesses').innerHTML = "Guesses left: " + totalGuesses;
+            document.getElementById('losses').innerHTML = "losses: " + losestotal;
+            document.getElementById('totalGuesses').innerHTML = "Total Guesses so far: " + guessesLeftTotal;
             }
             
     
